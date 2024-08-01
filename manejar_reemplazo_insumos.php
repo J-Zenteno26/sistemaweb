1992d250eb807b20e692938ea64914f27a5e7678
admin <zentenoestefania6@gmail.com>
2024-08-01 13:25:50 -0400

    01-08

--- manejar_reemplazo_insumos.php
+++ manejar_reemplazo_insumos.php
@@ -1,56 +1,43 @@
 <?php
-include ("administrador/config/bd.php");
 
+include ("administrador/config/bd.php");
 function obtenerCoberturasDisponibles($conexion) {
-    $tipo_insumo = 1; // Supongamos que el tipo de insumo para coberturas es 1
     $consulta = $conexion->prepare("
         SELECT insumo.id_insumo, insumo.nombre_insumo 
         FROM insumo 
         INNER JOIN stock ON insumo.id_insumo = stock.id_insumo
-        WHERE insumo.tipo_insumo = :tipo_insumo AND stock.porcion > 0
-    ");
-    $consulta->bindParam(':tipo_insumo', $tipo_insumo, PDO::PARAM_INT);
+        WHERE insumo.tipo_insumo = 1 AND stock.porcion > 0");
     $consulta->execute();
 
-    $coberturas = [];
-    while ($row = mysqli_fetch_assoc($consulta)) {
-        $coberturas[$row['id_cobertura']] = $row['nombre_cobertura'];
-    }
+    $coberturas = $consulta->fetchAll(PDO::FETCH_ASSOC);
     return $coberturas;
 }
 
 function obtenerProteinasDisponibles($conexion) {
-    $tipo_insumo = 2; // Supongamos que el tipo de insumo para proteínas es 2
     $consulta = $conexion->prepare("
         SELECT insumo.id_insumo, insumo.nombre_insumo 
         FROM insumo 
         INNER JOIN stock ON insumo.id_insumo = stock.id_insumo
-        WHERE insumo.tipo_insumo = :tipo_insumo AND stock.porcion > 0
-    ");
-    $consulta->bindParam(':tipo_insumo', $tipo_insumo, PDO::PARAM_INT);
+        WHERE insumo.tipo_insumo = 2 AND stock.porcion > 0");
     $consulta->execute();
 
-    $proteinas = [];
-
-    while ($row = mysqli_fetch_assoc($consulta)) {
-        $proteinas[$row['id_proteina']] = $row['nombre_proteina'];
-    }
+    $proteinas = $consulta->fetchAll(PDO::FETCH_ASSOC);
     return $proteinas;
-
 }
 
 function obtenerVegetalesDisponibles($conexion) {
-    $tipo_insumo = 3; // Supongamos que el tipo de insumo para vegetales es 3
     $consulta = $conexion->prepare("
         SELECT insumo.id_insumo, insumo.nombre_insumo 
         FROM insumo 
         INNER JOIN stock ON insumo.id_insumo = stock.id_insumo
-        WHERE insumo.tipo_insumo = :tipo_insumo AND stock.porcion > 0
-    ");
-    $consulta->bindParam(':tipo_insumo', $tipo_insumo, PDO::PARAM_INT);
+        WHERE insumo.tipo_insumo = 3 AND stock.porcion > 0");
     $consulta->execute();
-    return $consulta->fetchAll(PDO::FETCH_ASSOC);
+
+    $vegetales = $consulta->fetchAll(PDO::FETCH_ASSOC);
+    return $vegetales;
 }
+
+
 
 if (isset($_POST['action']) && $_POST['action'] == 'get_reemplazo') {
     $id_roll = $_POST['id_roll'];
@@ -64,9 +51,9 @@ if (isset($_POST['action']) && $_POST['action'] == 'get_reemplazo') {
             $insumos_disponibles = obtenerCoberturasDisponibles($conexion);
         } elseif (strpos($insumo, 'proteina') !== false) {
             $insumos_disponibles = obtenerProteinasDisponibles($conexion);
-        } elseif (strpos($insumo, 'vegetal') !== false) {
+        } elseif (strpos($insumo, 'vegetal_1') !== false || strpos($insumo, 'vegetal_2') !== false) {
             $insumos_disponibles = obtenerVegetalesDisponibles($conexion);
-        }
+        }        
 
         $output .= '<div class="form-group">';
         $output .= '<label for="insumo_' . $insumo . '">Reemplazar ' . $insumo . ' con :</label>';
@@ -80,4 +67,5 @@ if (isset($_POST['action']) && $_POST['action'] == 'get_reemplazo') {
 
     echo $output;
 }
+
 ?>
